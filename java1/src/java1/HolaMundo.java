7d600d40fd31ae5b4b7a93f6ff568d5c98238916
fran <ctefran@hotmail.com>
2022-09-21 20:36:25 +0200

    Java1

---
+++ java1/src/java1/HolaMundo.java
@@ -0,0 +1,10 @@
+package java1;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+		System.out.println("Adiós mundo cruel!");
+	}
+	
+}
