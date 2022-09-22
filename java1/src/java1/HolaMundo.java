abf67a5f2602891cfcc7357d674d3ae67bcdbd4d
fran <ctefran@hotmail.com>
2022-09-22 02:53:16 +0200

    Hola Mundo actualizado

--- java1/src/java1/HolaMundo.java
+++ java1/src/java1/HolaMundo.java
@@ -3,8 +3,20 @@ package java1;
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
-		System.out.println("Adiós mundo cruel!");
+		
+		int numero = 10;
+		int numero1 = 5;
+		int numero_2 = 8;
+		int _numero3 = 4;
+		
+		// Este es mi primer programa
+		// Solamente saca por pantalla mensajes
+		//System.out.println("Hola mundo!");
+		System.out.println("Adiós mundo cruel!");  // esta linea imprime por pantalla
+		/*System.out.println("Tercera linea");
+		System.out.println("Cuarta linea");*/
+		System.out.println(numero);
+		System.out.println(numero1+numero+numero_2+_numero3);
 	}
 	
 }
