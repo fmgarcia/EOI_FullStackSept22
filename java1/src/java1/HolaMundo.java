71a20f4bd5eabb3a5d1ea4d864d2437cc1045e19
fran <ctefran@hotmail.com>
2022-09-22 15:58:42 +0200

    Actualizar HolaMundo

--- java1/src/java1/HolaMundo.java
+++ java1/src/java1/HolaMundo.java
@@ -8,6 +8,15 @@ public class HolaMundo {
 		int numero1 = 5;
 		int numero_2 = 8;
 		int _numero3 = 4;
+		int suma = 0;
+		double numeroConDecimales = 3.5;
+		double numeroConDecimales2 = 4.4;
+		char letra = 'a';
+		String cadena = "Esto es una cadena";
+		String cadenaA = "a";
+		String cadenaVacia = "";
+		String cadena2 = "Esta es una segunda cadena";
+		
 		
 		// Este es mi primer programa
 		// Solamente saca por pantalla mensajes
@@ -17,6 +26,18 @@ public class HolaMundo {
 		System.out.println("Cuarta linea");*/
 		System.out.println(numero);
 		System.out.println(numero1+numero+numero_2+_numero3);
+		suma = numero + numero1;  // 15
+		System.out.println(suma);
+		System.out.println(numeroConDecimales+numeroConDecimales2);
+		System.out.println(numero1 + numeroConDecimales);  // 8.5
+		System.out.println(letra);
+		System.out.println(cadena);
+		System.out.println(letra + numero);  // 107??? -> a es 97 en ASCII
+		System.out.println(cadenaA + numero); // a10. Ha concatenado la cadena y el número
+		System.out.println(cadena + " " + cadena2);
+		System.out.println(cadena + '\n' + cadena2);
+		System.out.println(cadena + '\t' + cadena2);
+		
 	}
 	
 }
