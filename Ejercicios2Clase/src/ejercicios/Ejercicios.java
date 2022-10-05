4446af4ed69e85c94293559fc9247febf40b5a88
fran <ctefran@hotmail.com>
2022-10-05 18:03:25 +0200

    Ejercicio11

--- Ejercicios2Clase/src/ejercicios/Ejercicios.java
+++ Ejercicios2Clase/src/ejercicios/Ejercicios.java
@@ -48,6 +48,92 @@ public class Ejercicios {
 		sc.close();
 	}
 	
+	
+	public static void ejercicio11() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduzca el primer número: ");
+		int numero1 = sc.nextInt();
+		System.out.print("Introduzca el segundo número: ");
+		int numero2 = sc.nextInt();
+		System.out.println("1. Sumar");
+		System.out.println("2. Restar");
+		System.out.println("3. Multiplicar");
+		System.out.println("4. Dividir");			
+		System.out.println("Introduzca opción: ");
+		int opcion = sc.nextInt();
+		switch(opcion) {	// tratar la opción
+		case 1:
+			System.out.println("La suma es: " + (numero1+numero2));
+			break;
+		case 2:
+			System.out.println("La resta es: " + (numero1-numero2));
+			break;
+		case 3:
+			System.out.println("La multiplicación es: " + (numero1*numero2));
+			break;
+		case 4:
+			if(numero2!=0) {
+				System.out.println("La division es: " + (numero1/numero2));
+			} else {  // es igual a 0
+				System.out.println("No se puede dividir por cero");
+			}
+			break;
+		default:
+			System.out.println("Opción no válida.");
+			break;
+		}
+		
+	}
+	
+	public static void ejercicio11CalcularaBasica() {
+		Scanner sc = new Scanner(System.in);
+		int opcion = -1;
+
+		while (opcion!=0) {
+
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("0. Salir");
+			System.out.println("Introduzca opción: ");
+			opcion = sc.nextInt();
+			int numero1=-1;
+			int numero2=-1;
+			if (opcion>=1 && opcion<=4) {  // Si una operación que requiere números, los pedimos.
+				System.out.print("Introduzca el primer número: ");
+				numero1 = sc.nextInt();
+				System.out.print("Introduzca el segundo número: ");
+				numero2 = sc.nextInt();
+			}
+			switch (opcion) { // tratar la opción
+			case 1:
+				System.out.println("La suma es: " + (numero1 + numero2));
+				break;
+			case 2:
+				System.out.println("La resta es: " + (numero1 - numero2));
+				break;
+			case 3:
+				System.out.println("La multiplicación es: " + (numero1 * numero2));
+				break;
+			case 4:
+				if (numero2 != 0) {
+					System.out.println("La division es: " + (numero1 / numero2));
+				} else { // es igual a 0
+					System.out.println("No se puede dividir por cero");
+				}
+				break;
+			case 0:
+				System.out.println("Gracias por usar nuestra calculadora básica");
+				break;
+			default:
+				System.out.println("Opción no válida.");
+				break;
+			}
+		}
+		
+	}
+	
 	public static void ejercicio13() {
 		for (int i = 20; i > 0; i--) {
 			System.out.println(i);
@@ -57,7 +143,10 @@ public class Ejercicios {
 	public static void main(String[] args) {
 		//ejercicio2();
 		//ejercicio3();
-		ejercicio4();
+		//ejercicio4();
+		//ejercicio11();
+		ejercicio11CalcularaBasica();
+		
 	}
 
 }
